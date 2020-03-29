a9062893a99d771aeaa9833c12fda4995d723f00
Pavel <pavellevchukbg@gmail.com>
2020-03-29 15:56:44 +0300

    Updates

---
+++ precache-manifest.d5a1a39c732db43e81c8b2717fef4f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc98a82d038c67d70816acd9779f276",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "4ff65307927f0f3ca8e9",
+    "url": "/pokemons/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "572a10d492f90ae9978c",
+    "url": "/pokemons/static/css/main.afaa4ec2.chunk.css"
+  },
+  {
+    "revision": "4ff65307927f0f3ca8e9",
+    "url": "/pokemons/static/js/2.ff77830d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokemons/static/js/2.ff77830d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572a10d492f90ae9978c",
+    "url": "/pokemons/static/js/main.2c3cc0b7.chunk.js"
+  },
+  {
+    "revision": "6ad11756470c9a31f899",
+    "url": "/pokemons/static/js/runtime-main.af5f21ac.js"
+  }
+]);
